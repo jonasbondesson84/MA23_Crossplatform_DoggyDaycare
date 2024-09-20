e478349de8239bfc0b07095c6baa17bd5abeb0c6
jonasbondesson84 <jonasbondesson@yahoo.com>
2024-09-20 10:32:55 +0200

    fixed a bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,12 @@ import logo from '../assets/logo.webp'
 const Home = ({dogCatalog}) => {
     return ( 
         <div className="home">
-        <p id='title'>Welcome to Doggy Daycare</p>
-        <p >Dogs registered: {dogCatalog.length} </p>
-        <p>Dogs currently present: {dogCatalog.filter(dog => dog.present).length}</p>
+            {dogCatalog && <div>
+                <p id='title'>Welcome to Doggy Daycare</p>
+                <p >Dogs registered: {dogCatalog.length} </p>
+                <p>Dogs currently present: {dogCatalog.filter(dog => dog.present).length}</p>
+                </div>
+            }
         </div>
      );
 }
