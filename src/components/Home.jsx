0111ab1413b0b2c7b2d1fa5e3e151d3c265c8e8f
jonasbondesson84 <jonasbondesson@yahoo.com>
2024-09-20 10:04:08 +0200

    fixed home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import logo from '../assets/logo.webp'
 
-const Home = () => {
+const Home = ({dogCatalog}) => {
     return ( 
-        <div className="home">Home
-        <img src={logo} alt="" />
+        <div className="home">
+        <p id='title'>Welcome to Doggy Daycare</p>
+        <p >Dogs registered: {dogCatalog.length} </p>
+        <p>Dogs currently present: {dogCatalog.filter(dog => dog.present).length}</p>
         </div>
      );
 }
